68ef2e5cdc0ab68ccdd40fbe5a91aaec899fa41e
bill <jinbiao.zang@j-make.cn>
2019-11-18 14:44:30 +0800

    获取github的id

---
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -0,0 +1,31 @@
+package life.majiang.community.controller;
+
+import life.majiang.community.dto.AccessTokenDTO;
+import life.majiang.community.dto.GithubUser;
+import life.majiang.community.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name = "code") String code,
+                           @RequestParam(name = "state") String state) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setRedirect_uri("http://localhost/callback");
+        accessTokenDTO.setClient_id("be820bf954481a9efcd7");
+        accessTokenDTO.setClient_secret("931f78470980135ef1a941526fcfcdfcd02ebd80");
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser user = githubProvider.getUser(accessToken);
+        System.out.println(user.toString());
+        return "index";
+    }
+}
