6a97413acc54c83291c64d9ca4e37f16440fd824
bill <jinbiao.zang@j-make.cn>
2019-11-21 18:02:56 +0800

    --add

---
+++ src/main/java/life/majiang/community/dto/PaginationDTO.java
@@ -0,0 +1,65 @@
+package life.majiang.community.dto;
+
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class PaginationDTO {
+    private List<QuestionDTO> questionDTOS;
+    private Boolean showPrevious;
+    private Boolean showFirstPage;
+    private Boolean showNext;
+    private Boolean showEndPage;
+
+    private Integer page;
+
+    private List<Integer> pages = new ArrayList<>();
+
+    public void setPagination(Integer totalCount, Integer page, Integer size) {
+
+        Integer totalPage;
+        if (totalCount % size == 0) {
+            totalPage = totalCount / size;
+        } else {
+            totalPage = totalCount / size + 1;
+        }
+        pages.clear();
+        pages.add(page);
+        for (int i = 1; i <= 3; i++) {
+            if (page - i > 0) {
+                pages.add(0, page - i);
+            }
+            if (page + i <= totalPage) {
+                pages.add(page + i);
+            }
+        }
+
+
+        if (page == 1) {
+            showPrevious = false;
+        } else {
+            showPrevious = true;
+        }
+
+        if (page == totalPage) {
+            showNext = false;
+        } else {
+            showNext = true;
+        }
+
+        if (pages.contains(1)) {
+            showFirstPage = false;
+        } else {
+            showFirstPage = true;
+        }
+        if (pages.contains(totalCount)) {
+            showEndPage = false;
+        } else {
+            showEndPage = true;
+        }
+
+
+    }
+}
