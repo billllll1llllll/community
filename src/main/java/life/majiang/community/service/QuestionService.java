f6594facdcdcd34e9abee2373cee60b8b4d46615
bill <jinbiao.zang@j-make.cn>
2019-12-02 18:29:51 +0800

    -- ++viewCount

--- src/main/java/life/majiang/community/service/QuestionService.java
+++ src/main/java/life/majiang/community/service/QuestionService.java
@@ -5,6 +5,7 @@ import life.majiang.community.dto.PaginationDTO;
 import life.majiang.community.dto.QuestionDTO;
 import life.majiang.community.exception.CustomizeErrorCode;
 import life.majiang.community.exception.CustomizeException;
+import life.majiang.community.mapper.QuestionExtMapper;
 import life.majiang.community.mapper.QuestionMapper;
 import life.majiang.community.mapper.UserMapper;
 import life.majiang.community.model.Question;
@@ -23,6 +24,8 @@ public class QuestionService {
 
     @Autowired(required = false)
     private QuestionMapper questionMapper;
+    @Autowired(required = false)
+    private QuestionExtMapper questionExtMapper;
     @Autowired(required = false)
     private UserMapper userMapper;
 
@@ -132,5 +135,15 @@ public class QuestionService {
 
         }
 
+    }
+
+    /**
+     * 浏览数+1
+     */
+    public void incView(Integer id) {
+        Question question = new Question();
+        question.setId(id);
+        question.setViewCount(1);
+        questionExtMapper.incView(question);
     }
 }
