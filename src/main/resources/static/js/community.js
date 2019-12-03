aacc43c5e3d3a7976ff4408eb89426dd5fe377d8
bill <jinbiao.zang@j-make.cn>
2019-12-03 19:28:41 +0800

    --回复

---
+++ src/main/resources/static/js/community.js
@@ -0,0 +1,36 @@
+function post() {
+    var questionId = $("#question_id").val()
+    var questionContent = $("#question_content").val()
+
+    $.ajax({
+        type: "POST",
+        url: "/comment",
+        contentType: "application/json",
+        data: JSON.stringify({
+            "parentId": questionId,
+            "content": questionContent,
+            "type": 1
+        }),
+        success: function (response) {
+            if (response.code == 200) {
+                $("#comment_section").hide()
+            } else {
+                if (response.code == 2003) {
+                    var isAccept = confirm(response.message)
+                    if(isAccept){
+
+                    }else {
+                        window.open("https://github.com/login/oauth/authorize?client_id=be820bf954481a9efcd7&redirect_uri=http://localhost/callback&scope=user&state=1")
+                        window.localStorage.setItem("cloasable",true)
+                    }
+                } else {
+                    alert(response.message)
+                }
+            }
+            console.log(response)
+        },
+        dataType: "json"
+    })
+
+
+}
