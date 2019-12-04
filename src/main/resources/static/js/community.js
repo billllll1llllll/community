f43b1e90187ada2caad05a27e2e1a9f89f4bd040
bill <jinbiao.zang@j-make.cn>
2019-12-04 15:50:23 +0800

    --回复

--- src/main/resources/static/js/community.js
+++ src/main/resources/static/js/community.js
@@ -2,6 +2,10 @@ function post() {
     var questionId = $("#question_id").val()
     var questionContent = $("#question_content").val()
 
+    if (!questionContent) {
+        alert("不能回复空内容")
+        return
+    }
     $.ajax({
         type: "POST",
         url: "/comment",
@@ -13,15 +17,16 @@ function post() {
         }),
         success: function (response) {
             if (response.code == 200) {
-                $("#comment_section").hide()
+                // $("#comment_section").hide()
+                window.location.reload();
             } else {
                 if (response.code == 2003) {
                     var isAccept = confirm(response.message)
-                    if(isAccept){
+                    if (isAccept) {
 
-                    }else {
+                    } else {
                         window.open("https://github.com/login/oauth/authorize?client_id=be820bf954481a9efcd7&redirect_uri=http://localhost/callback&scope=user&state=1")
-                        window.localStorage.setItem("cloasable",true)
+                        window.localStorage.setItem("cloasable", true)
                     }
                 } else {
                     alert(response.message)
